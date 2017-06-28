907facb5d5e35f5732fcd638990ee813fabe6fdb
mbroons <mbrooons@gmail.com>
2017-06-28 10:22:52 +0200

    functions and classes

---
+++ src/classes/Classes.kt
@@ -0,0 +1,98 @@
+package classes
+
+import java.util.*
+
+// No constructor
+class Customer {
+    // No such thing as field. rather properties
+    var id = 0
+    var name: String = ""
+}
+
+// Another version with a constructor with mutable properties
+class CustomerVersion1(var id: Int, var name: String) {
+
+}
+
+// Another version with a constructor with immutable properties
+class CustomerVersion2(val id: Int, val name: String) {
+
+}
+
+// Another version with default vamues and initialization block
+class CustomerVersion3(var id: Int, var name: String = "") {
+    init {
+        name = name.toUpperCase()
+    }
+
+    // secondary constructors extending primary ones...
+    // I don't really get it. What is the purpose? is email a property ? nope!
+    // So what is the use then...
+    constructor(email: String) : this(0, "") {
+
+    }
+}
+
+// Class with custom getter and setter for properties
+// introduce methods too!
+class CustomerVersion4(var id: Int, var name: String = "", val yearOfBirth: Int) {
+
+    val age: Int
+        get() = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth
+
+    var socialSecurityNumber: String = ""
+        set(value) {
+            if (!value.startsWith("SN")) {
+                throw IllegalArgumentException("Social security number must start with SN")
+            }
+            // Don't forget to set the property backing field!
+            field = value
+        }
+
+    fun printCustomerAsString() {
+        println("id: $id, name: $name, age: $age, socialSecurityNumber: $socialSecurityNumber")
+    }
+}
+
+fun main(args: Array<String>) {
+
+    // No new keyword !!
+    var roger = Customer()
+    roger.id = 10
+    roger.name = "roger"
+
+    var rogerVersion1 = CustomerVersion1(1, "roger")
+    rogerVersion1.id = 2
+
+    var rogerVersion2 = CustomerVersion2(3, "roger")
+    // Can't do that!
+    // rogerVersion2.id = 4
+
+    var rogerVersion3 = CustomerVersion3(4)
+
+    var thomas = CustomerVersion4(5, "thomas", 1968)
+    // thomas.socialSecurityNumber = "1680869blablabla"
+    thomas.socialSecurityNumber = "SNblablabla"
+    println(thomas.socialSecurityNumber)
+    thomas.printCustomerAsString()
+}
+
+/*
+About visibility:
+
+by default everything is public.
+
+top level declaration visibility (classes, functions)
+
+public:
+private: only visible in the file where it is declared
+internal: only visible in the same module (what does it mean module?
+
+for class proeprties
+
+public
+private: only available to class menbers
+protected: only available to class menbers and subclasses
+internal: any client inside the module!
+
+ */
--- src/function/Functions.kt
+++ src/function/Functions.kt
@@ -32,9 +32,56 @@ fun sum(x :Int, y :Int):Int {
 // Condensed version with type inference
 fun anotherSum(x: Int, y :Int) = x + y
 
+// default parameters
+fun anotherSumWithDefault(x: Int, y :Int, z :Int = 0) = x + y + z
+
+fun printDetails(name :String, email :String = "", phone :String) {
+    println("name: $name, email: $email, phone: $phone");
+}
+
+fun printStrings(vararg strings: String) {
+
+    for (string in strings) {
+        println(string)
+    }
+}
+
+// Caution when passing in varard
+
+fun anotherPrintStrings(vararg strings: String) {
+
+    // This does not work
+    // reallyPrintStrings(strings)
+
+    // I need to specify the spread operator to make it work
+    reallyPrintStrings(*strings)
+}
+
+// package private functions
+private fun reallyPrintStrings(vararg strings: String) {
+    for (string in strings) {
+        println(string)
+    }
+}
+
+
 fun main(args: Array<String>) {
     hello()
     anotherHello()
     val value = returns4()
     println(anotherSum(3, 4))
+
+    // Call function with default. Avoid overloading functions
+    println(anotherSum(3, 4))
+
+    // Using named parameters
+    printDetails("Roger", phone = "01 55 66 55 55")
+    // Change the parameters order.
+    // Use with care, usually it is better to have a data object in such case!
+    printDetails("Roger", phone = "01 55 66 55 55", email = "roger.gicquel@gmail.com")
+
+    printStrings("1")
+    printStrings("1", "2") // Nice tip here! Ctrl D to duplicate the line
+    printStrings("1", "2", "3")
+    printStrings("1", "2", "3", "4")
 }
