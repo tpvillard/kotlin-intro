d5d72f670061e3723f4a84110f8c9faba2f24918
mbroons <mbrooons@gmail.com>
2018-01-29 22:31:33 +0100

    Advanced programming: local returns

---
+++ src/main/kotlin/advanced/functions/ReturnsAndLocalReturns.kt
@@ -0,0 +1,10 @@
+package advanced.functions
+
+/**
+ * I need to provide some code here but basically:
+ * local return are not possible from lambda unless you use a label.
+ * (return from a lambda will return from the containing function)
+ * local returns are possible from anonymous functions.
+ *
+ * The question is: do I really need this local return feature?
+ */
